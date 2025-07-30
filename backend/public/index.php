4ddd123903b457f9f104a8ce3876d1216dc193e3
Edson Pimenta <dev.edsonpimenta@gmail.com>
2025-07-30 10:43:01 -0300

    refactor(api): moves all slim app calls to /api

--- backend/public/index.php
+++ backend/public/index.php
@@ -8,9 +8,27 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->setBasePath('/api');
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello from Slim in /api");
     return $response;
 });
+
+// Test endpoints
+$app->get('/ping', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write(json_encode(['message' => 'pong']));
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(200);
+});
+
+$app->get('/ping/{name}', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write(json_encode(['message' => 'pong for ' . $args['name']]));
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(200);
+});
+// End test endpoints
 
 $app->run();
